bee6db0bbcc0c0d1fb125b6a92d98ee1882deca9
Mike <mike.castellon5@gmail.com>
2021-04-06 15:18:52 -0400

    initial commit, installed web3, setup web3.js, set lottery main page outline

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import web3 from './web3';
+import lottery from './lottery'
 
 function App() {
+  const [manager, setManager] = useState('')
+
+  useEffect(() => {
+    async function getManager(){
+      const manager = await lottery.methods.manager().call()
+
+      setManager(manager)
+    }
+
+    getManager()
+  })
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Lottery Contract</h2>
+      <p>This Contract is managed by: {manager}</p>  
     </div>
   );
 }
